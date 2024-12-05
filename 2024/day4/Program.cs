2b0af61c3fb29011f5204f8f4cc147d30471fbcb
Ismail Halili <i.halili99@hotmail.com>
2024-12-05 02:13:56 +0100

    add beginning of day4

---
+++ 2024/day4/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] grid = File.ReadAllLines("input.txt");
+int rows = grid.Length;
+int columns = grid[0].Length;
+List<Tuple<int, int>> directions = new()
+{
+    new Tuple<int, int>(-1, 0),  // Up
+    new Tuple<int, int>(1, 0),   // Down
+    new Tuple<int, int>(0, -1),  // Left
+    new Tuple<int, int>(0, 1),   // Right
+    new Tuple<int, int>(-1, -1), // Up-Left
+    new Tuple<int, int>(-1, 1),  // Up-Right
+    new Tuple<int, int>(1, -1),  // Down-Left
+    new Tuple<int, int>(1, 1)    // Down-Right
+};
+
+for(int r = 0; r < rows; r++) {
+    for(int c = 0; c < rows; c++) {
+      if(grid[r][c] == 'X') {
+        foreach(var d in directions) {
+          // while loop? going so far bis gefunden 
+        }
+      }
+    }
+}
