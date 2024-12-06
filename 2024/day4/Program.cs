b711670ef6e4e6526a44e6e802d549d0fe7a784e
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2024-12-06 02:23:35 +0100

    add day 4

--- 2024/day4/Program.cs
+++ 2024/day4/Program.cs
@@ -3,22 +3,56 @@ int rows = grid.Length;
 int columns = grid[0].Length;
 List<Tuple<int, int>> directions = new()
 {
-    new Tuple<int, int>(-1, 0),  // Up
-    new Tuple<int, int>(1, 0),   // Down
-    new Tuple<int, int>(0, -1),  // Left
-    new Tuple<int, int>(0, 1),   // Right
-    new Tuple<int, int>(-1, -1), // Up-Left
-    new Tuple<int, int>(-1, 1),  // Up-Right
-    new Tuple<int, int>(1, -1),  // Down-Left
-    new Tuple<int, int>(1, 1)    // Down-Right
+    new Tuple<int, int>(-1, 0),  
+    new Tuple<int, int>(1, 0),   
+    new Tuple<int, int>(0, -1),  
+    new Tuple<int, int>(0, 1),   
+    new Tuple<int, int>(-1, -1),
+    new Tuple<int, int>(-1, 1), 
+    new Tuple<int, int>(1, -1), 
+    new Tuple<int, int>(1, 1)   
 };
+int ans = 0;
+int ans2 = 0;
 
 for(int r = 0; r < rows; r++) {
     for(int c = 0; c < rows; c++) {
       if(grid[r][c] == 'X') {
         foreach(var d in directions) {
-          // while loop? going so far bis gefunden 
+          string mas = "";
+          int step = 1;
+          while(mas.Length < 3) {
+            try {
+              mas += grid[r + d.Item1 * step][c + d.Item2 * step];
+              step++;
+            } catch(IndexOutOfRangeException) {
+              break;
+            }
+          }
+          if(mas == "MAS") {
+            ans++;
+          }
         }
       }
+
+      if(grid[r][c] == 'A') {
+            try {
+                string corners = "";
+                corners += grid[r+1][c-1];
+                corners += grid[r+1][c+1];
+                corners += grid[r-1][c+1];
+                corners += grid[r-1][c-1];
+
+                if(corners == "MMSS" || corners == "MSSM" || corners == "SMMS" || corners == "SSMM") {
+                    ans2++;
+                }
+ 
+            } catch(IndexOutOfRangeException) {
+                continue;
+            }
+      } 
     }
 }
+
+Console.WriteLine(ans);
+Console.WriteLine(ans2);
